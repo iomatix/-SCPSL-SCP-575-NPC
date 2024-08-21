fcd2f0469b6a8af83dc2af3fa51524f1f2e477d5
iomatix <mateuszwypchlak@gmail.com>
2024-08-21 19:01:25 +0200

    Added blood

--- SCP-575/Npc/Methods.cs
+++ SCP-575/Npc/Methods.cs
@@ -335,6 +335,7 @@ namespace SCP_575.Npc
                     if (player.IsHuman && player.CurrentRoom.AreLightsOff && !player.HasFlashlightModuleEnabled && !(player.CurrentItem?.IsEmittingLight ?? false))
                     {
                         player.Hurt(Config.KeterDamage * blackoutStacks, Config.KilledBy);
+                        player.PlaceBlood(new Vector3(0f,0f));
                         player.Broadcast(Config.KeterBroadcast);
                     }
 
