fe034be209240a3f17c3d189cf1250ee422f1caf
iomatix <mateuszwypchlak@gmail.com>
2025-04-28 20:05:19 +0200

    Libs updated to Exiled 9.5.2

--- SCP-575/Plugin.cs
+++ SCP-575/Plugin.cs
@@ -17,7 +17,7 @@ namespace SCP_575
         public override string Prefix { get; } = "SCP575";
 
 
-        public override Version Version { get; } = new(6, 3, 0);
+        public override Version Version { get; } = new(6, 3, 2);
         public override Version RequiredExiledVersion { get; } = new(9, 5, 0);
 
         public EventHandlers EventHandlers { get; private set; }
