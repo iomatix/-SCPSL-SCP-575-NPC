938ace0161e9bffab20e3cce621f92189390c737
iomatix <mateuszwypchlak@gmail.com>
2025-07-19 12:40:42 +0200

    Debug improv

--- SCP-575/Shared/Scp575Helpers.cs
+++ SCP-575/Shared/Scp575Helpers.cs
@@ -15,9 +15,9 @@
 
         public static Scp575ImpactType ClassifyProjectileImpact(LabApi.Features.Wrappers.TimedGrenadeProjectile projectile)
         {
-            if (projectile == null)
-                return Scp575ImpactType.Unknown;
+            if (projectile == null) return Scp575ImpactType.Unknown;
 
+            Library_ExiledAPI.LogDebug("Scp575ImpactType", $"ItemType is {projectile.Type}");
             return projectile.Type switch
             {
                 // ToDo: Blocked for now. Its workaround from this known issue (https://github.com/northwood-studios/LabAPI/issues/219#issuecomment-3091106438)
@@ -32,9 +32,9 @@
 
         public static Scp575ImpactType ClassifyExplosionImpact(ExplosionType type)
         {
-            if (type == null)
-                return Scp575ImpactType.Unknown;
+            if (type == null) return Scp575ImpactType.Unknown;
 
+            Library_ExiledAPI.LogDebug("Scp575ImpactType", $"ExplosionType is {type}");
             return type switch
             {
                 ExplosionType.Grenade => Scp575ImpactType.Dangerous,
