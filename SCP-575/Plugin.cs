2cd979f79906f02ca6142b6fd66de48879bff738
iomatix <mateuszwypchlak@gmail.com>
2025-07-29 20:45:50 +0200

    Working build 8.6.1

--- SCP-575/Plugin.cs
+++ SCP-575/Plugin.cs
@@ -80,7 +80,7 @@ namespace SCP_575
         /// <summary>
         /// Gets the version of the plugin.
         /// </summary>
-        public override System.Version Version => new(8,6,0);
+        public override System.Version Version => new(8,6,1);
 
         /// <summary>
         /// Gets the minimum required Exiled version for compatibility.
