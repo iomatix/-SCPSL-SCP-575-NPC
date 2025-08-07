6755371b86d126a3529906a071992a167485e2b8
iomatix <mateuszwypchlak@gmail.com>
2025-08-07 18:49:52 +0200

    8.6.7 RC

--- SCP-575/Plugin.cs
+++ SCP-575/Plugin.cs
@@ -80,7 +80,7 @@ namespace SCP_575
         /// <summary>
         /// Gets the version of the plugin.
         /// </summary>
-        public override System.Version Version => new(8,6,6);
+        public override System.Version Version => new(8,6,7);
 
         /// <summary>
         /// Gets the minimum required Exiled version for compatibility.
