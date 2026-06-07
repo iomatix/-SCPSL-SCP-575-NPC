75486903546798aae7eaa4d25d5c73d5dffa585f
iomatix <mateuszwypchlak@gmail.com>
2026-06-07 17:44:15 +0200

    9.4.3

--- SCP-575/Plugin.cs
+++ SCP-575/Plugin.cs
@@ -42,7 +42,7 @@ namespace SCP_575
         public override string Author => "iomatix";
         public override string Name => "SCP-575 NPC";
         public override string Prefix => "SCP575";
-        public override System.Version Version => new(9, 4, 2);
+        public override System.Version Version => new(9, 4, 3);
         public override System.Version RequiredExiledVersion => new(9, 9, 3);
 
         public override void OnEnabled()
