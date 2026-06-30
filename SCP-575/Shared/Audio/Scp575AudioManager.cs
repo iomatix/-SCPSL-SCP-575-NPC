32f807dde146993b9b110ce9da8db09eee8378a6
iomatix <mateuszwypchlak@gmail.com>
2026-06-30 20:06:20 +0200

    Priority change

--- SCP-575/Shared/Audio/Scp575AudioManager.cs
+++ SCP-575/Shared/Audio/Scp575AudioManager.cs
@@ -351,7 +351,7 @@
             _audioRegistry[AudioKey.ShadowConsumingBody] = new AudioTrackGroup(new AudioTrackProfile("scp575.shadow_consuming_body", 0.95f, 7.5f, 45f, true, AudioPriority.High, 5f));
             _audioRegistry[AudioKey.AnomalousImpact] = new AudioTrackGroup(new AudioTrackProfile("scp575.anomalous_impact", 0.9f, 3.5f, 25f, true, AudioPriority.High, 5f));
             _audioRegistry[AudioKey.GeneratorHumDefense] = new AudioTrackGroup(new AudioTrackProfile("scp575.generator_hum_defense", 0.7f, 6.5f, 45f, true, AudioPriority.Medium, 0f));
-            _audioRegistry[AudioKey.LightShortCircuit] = new AudioTrackGroup(new AudioTrackProfile("scp575.light_short_circuit", 0.75f, 2.5f, 15f, true, AudioPriority.Max, 1.5f));
+            _audioRegistry[AudioKey.LightShortCircuit] = new AudioTrackGroup(new AudioTrackProfile("scp575.light_short_circuit", 0.75f, 2.5f, 15f, true, AudioPriority.High, 1.5f));
             _audioRegistry[AudioKey.LightSwitch] = new AudioTrackGroup(
                 new AudioTrackProfile("scp575.light_switch_1", 0.85f, 2.5f, 18f, true, AudioPriority.Max, 1.5f),
                 new AudioTrackProfile("scp575.light_switch_2", 0.85f, 2.5f, 18f, true, AudioPriority.Max, 1.5f),
